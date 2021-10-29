6603bb2c075be64081ecb43e7e11b0f15ef694c8
Jason <Jaytonbye@yahoo.com>
2021-10-29 13:34:59 -0400

    minor change again

--- src/client/PasswordReset.tsx
+++ src/client/PasswordReset.tsx
@@ -66,12 +66,6 @@ export default function PasswordReset() {
 
   return (
     <>
-      <h2>
-        We have not yet built password reset functionality. Please contact coach
-        Jason, he can help you reset your password. If you can't remember your
-        username, it is likely the wrestler's full name, in all lowercase
-        letters, without spaces.
-      </h2>
       <label>
         <strong>Email address: </strong>
       </label>
