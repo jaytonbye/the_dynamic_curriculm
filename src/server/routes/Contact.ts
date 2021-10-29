88830167ec6f910259b0399329065e267de57d8b
Jason <Jaytonbye@yahoo.com>
2021-10-29 12:28:14 -0400

    put it back, it didn't work

--- src/server/routes/Contact.ts
+++ src/server/routes/Contact.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as FormData from "form-data";
-
+//@ts-ignore
+import * as Mailgun from "mailgun.js";
 import MailGun from "mailgun.js";
 import config from "../config";
 
-const mailgun = new (<typeof MailGun>(<any>MailGun))(<any>FormData).client({
+const mailgun = new (<typeof MailGun>(<any>Mailgun))(<any>FormData).client({
   username: "api",
   key: config.mailgun.apiKey,
 });
