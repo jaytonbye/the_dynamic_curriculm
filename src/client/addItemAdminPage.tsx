48aaa04eaf377ca7a08a4729c0f39ae0e1b7776d
Jason <Jaytonbye@yahoo.com>
2021-12-30 10:16:32 -0500

    waynes code review checks out

--- src/client/addItemAdminPage.tsx
+++ src/client/addItemAdminPage.tsx
@@ -1,41 +1,37 @@
 // @ts-nocheck
-import React from 'react'
-import ShirtSelector from './ShirtSelector'
-import ShirtDisplayer from './ShirtDisplayer'
-import { useState, useEffect } from 'react';
+import React from "react";
+import ShirtSelector from "./ShirtSelector";
+import ShirtDisplayer from "./ShirtDisplayer";
+import { useState, useEffect } from "react";
 
-
-interface Props {
-
-}
-
-
+interface Props {}
 
 const addItemAdminPage: React.FC<Props> = () => {
+  const [clothing, setClothing] = useState("");
+  const [color, setColor] = useState("");
+  const [percent, setPercent] = useState<number>(0);
 
+  let STATES: any = {
+    clothing,
+    color,
+    percent,
+  };
 
-    const [clothing, setClothing] = useState("");
-    const [color, setColor] = useState("");
-    const [percent, setPercent] = useState<number | string>(0);
+  let SetSTATES: any = {
+    setClothing,
+    setColor,
+    setPercent,
+  };
 
-    let STATES: any = {
-        clothing,
-        color,
-        percent
-    }
-
-    let SetSTATES: any = {
-        setClothing,
-        setColor,
-        setPercent
-    }
-
-    return (
-        <div>
-            <ShirtSelector statesFromAddItemAdminPage={STATES} setStatesFromAddItemAdminPage={SetSTATES} />
-            <ShirtDisplayer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <ShirtSelector
+        statesFromAddItemAdminPage={STATES}
+        setStatesFromAddItemAdminPage={SetSTATES}
+      />
+      <ShirtDisplayer />
+    </div>
+  );
+};
 
 export default addItemAdminPage;
