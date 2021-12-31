33c6603cc9495164bb6c427b4dc662704a51b361
Jason <Jaytonbye@yahoo.com>
2021-12-31 11:02:52 -0500

    earnable items sorted by percentage

--- src/server/db/EarnableItemsQueries.ts
+++ src/server/db/EarnableItemsQueries.ts
@@ -1,50 +1,45 @@
 import { Query } from "./index";
 
-
-// INSERT INTO videos (name_of_video, url_to_video, url_to_looped_video, number_for_ordering, curriculum_level, maximum_grade, tenant) VALUES (?,?,?,?,?,?, 
+// INSERT INTO videos (name_of_video, url_to_video, url_to_looped_video, number_for_ordering, curriculum_level, maximum_grade, tenant) VALUES (?,?,?,?,?,?,
 //     (Select tenant from users Where id=?))`
 
 const selectAllFromEarnable = async (UID: string | number) => {
-    return Query(
-        `SELECT * from earnable_items where tenant = (Select tenant from users Where id=?)`,
-        [
-            UID
-        ]
-
-    );
+  return Query(
+    `SELECT * from earnable_items where tenant = (Select tenant from users Where id=?) ORDER BY percentage_of_total_points_needed`,
+    [UID]
+  );
 };
 
-const insertIntoEarnablItems = async (userId: number, userId2: number, itemName: string, itemColor: string, PercentagePointsNeeded: number) => {
-    return Query(
-        'INSERT INTO earnable_items (user_id, tenant, item_name, item_color, percentage_of_total_points_needed) VALUES (?, (Select tenant from users Where id=?), ?, ?, ?)',
-        [
-            userId, userId2, itemName, itemColor, PercentagePointsNeeded
-        ]
-    )
-}
+const insertIntoEarnablItems = async (
+  userId: number,
+  userId2: number,
+  itemName: string,
+  itemColor: string,
+  PercentagePointsNeeded: number
+) => {
+  return Query(
+    "INSERT INTO earnable_items (user_id, tenant, item_name, item_color, percentage_of_total_points_needed) VALUES (?, (Select tenant from users Where id=?), ?, ?, ?)",
+    [userId, userId2, itemName, itemColor, PercentagePointsNeeded]
+  );
+};
 
 const deleteFromEarnablItems = async (itemId: number | string) => {
-    return Query(
-        'delete from earnable_items where id = ?;',
-        [
-            itemId
-        ]
-    )
-}
+  return Query("delete from earnable_items where id = ?;", [itemId]);
+};
 
-const selectColumnEquals = async (column: string, columnEquals: string | number) => {
-    return Query(
-        `SELECT * from earnable_items where ??=?`,
-        [
-            column,
-            columnEquals
-        ]
-    );
+const selectColumnEquals = async (
+  column: string,
+  columnEquals: string | number
+) => {
+  return Query(`SELECT * from earnable_items where ??=?`, [
+    column,
+    columnEquals,
+  ]);
 };
 
 export default {
-    selectAllFromEarnable,
-    insertIntoEarnablItems,
-    deleteFromEarnablItems,
-    selectColumnEquals,
-}
+  selectAllFromEarnable,
+  insertIntoEarnablItems,
+  deleteFromEarnablItems,
+  selectColumnEquals,
+};
