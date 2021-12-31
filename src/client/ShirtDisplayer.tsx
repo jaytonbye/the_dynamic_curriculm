c152f9d0927648bd25931276170aaa44c4184ade
wakywayne <wakywayne80@gmail.com>
2021-12-31 11:36:50 -0500

    This is the repaired code

--- src/client/ShirtDisplayer.tsx
+++ src/client/ShirtDisplayer.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React from 'react'
+import { useState, useEffect } from 'react';
 
+<<<<<<< HEAD
 interface Props { }
+=======
+
+interface Props {
+
+}
+>>>>>>> parent of 48aaa04 (waynes code review checks out)
 
 const ShirtDisplayer: React.FC<Props> = () => {
   let UID = sessionStorage.getItem("UID");
@@ -15,16 +22,18 @@ const ShirtDisplayer: React.FC<Props> = () => {
     })
       .then((res) => res.json())
       .then((coding) => {
-        console.log(coding);
-        setItems(coding);
-        console.log(coding);
+        console.log(coding)
+        setItems(coding)
+        console.log(coding)
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+
+  }, [])
 
   const onDeleteItem = (id: number) => {
+    let token = sessionStorage.getItem("token");
     const requestOptions = {
       method: "DELETE",
       headers: {
@@ -37,16 +46,18 @@ const ShirtDisplayer: React.FC<Props> = () => {
     };
     fetch(`/api/earnableItems/delete/${id}`, requestOptions).then((res) => {
       if (res.ok) {
-        alert("Item deleted");
+        alert("Video deleted");
+        window.location.reload();
       } else {
         alert("it didn't work!");
       }
     });
   };
 
+
   return (
-    <div className="row">
-      <table className="table w-75 mx-auto">
+    <div className='row'>
+      <table className='table w-75 mx-auto'>
         <thead>
           <tr>
             <th>Id</th>
@@ -63,6 +74,14 @@ const ShirtDisplayer: React.FC<Props> = () => {
                 <td>{move.item_color}</td>
                 <td>{move.item_name}</td>
                 <td>{move.percentage_of_total_points_needed}</td>
+                {/* <td>
+                                    <button
+                                        className="btn btn-success"
+                                        onClick={() => onEditMove(move.id)}
+                                    >
+                                        Submit Update
+                                    </button>
+                                </td> */}
                 <td>
                   <button
                     className="btn btn-danger"
@@ -77,7 +96,7 @@ const ShirtDisplayer: React.FC<Props> = () => {
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
 export default ShirtDisplayer;
--- src/client/addItemAdminPage.tsx
+++ src/client/addItemAdminPage.tsx
@@ -1,39 +1,41 @@
 // @ts-nocheck
-import React from "react";
-import ShirtSelector from "./ShirtSelector";
-import ShirtDisplayer from "./ShirtDisplayer";
-import { useState, useEffect } from "react";
+import React from 'react'
+import ShirtSelector from './ShirtSelector'
+import ShirtDisplayer from './ShirtDisplayer'
+import { useState, useEffect } from 'react';
 
-interface Props { }
+
+interface Props {
+
+}
+
+
 
 const addItemAdminPage: React.FC<Props> = () => {
+
+
   const [clothing, setClothing] = useState("");
   const [color, setColor] = useState("");
-  const [percent, setPercent] = useState<number>(0);
-  const [incrementor, setIncrementor] = useState<number>(0);
-
+  const [percent, setPercent] = useState<number | string>(0);
 
   let STATES: any = {
     clothing,
     color,
-    percent,
-  };
+    percent
+  }
 
   let SetSTATES: any = {
     setClothing,
     setColor,
-    setPercent,
-  };
+    setPercent
+  }
 
   return (
     <div>
-      <ShirtSelector
-        statesFromAddItemAdminPage={STATES}
-        setStatesFromAddItemAdminPage={SetSTATES}
-      />
+      <ShirtSelector statesFromAddItemAdminPage={STATES} setStatesFromAddItemAdminPage={SetSTATES} />
       <ShirtDisplayer />
     </div>
-  );
-};
+  )
+}
 
 export default addItemAdminPage;
