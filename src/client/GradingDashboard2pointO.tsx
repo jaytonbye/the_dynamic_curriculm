c6773a7739d028e62f0d0e8896e45bb6d28f28b5
Jason <Jaytonbye@yahoo.com>
2022-03-25 14:05:50 -0400

    deleted 2 broken divs

--- src/client/GradingDashboard2pointO.tsx
+++ src/client/GradingDashboard2pointO.tsx
@@ -27,7 +27,6 @@ const defaultInfoState = {
 }; //
 
 const GradingDashboard2pointO: React.FC<Props> = () => {
-<<<<<<< HEAD
   let UID = sessionStorage.getItem("UID");
   let token = sessionStorage.getItem("token");
 
@@ -122,12 +121,15 @@ const GradingDashboard2pointO: React.FC<Props> = () => {
       theIndex++
     ) {
       const element = itemsSortedByPercentOfTotalPoints[theIndex];
-      if (element.percentage_of_total_points_needed >= totalPoints) {
+      if (element.percentage_of_total_points_needed > totalPoints) {
         setCurrentItem(
           `${itemsSortedByPercentOfTotalPoints[theIndex - 1].item_color} ${
             itemsSortedByPercentOfTotalPoints[theIndex - 1].item_name
           } `
         );
+        break;
+      } else if ((element.percentage_of_total_points_needed = totalPoints)) {
+        setCurrentItem(`${element}.item_color} ${element}.item_name} `);
         break;
       }
     }
@@ -166,137 +168,6 @@ const GradingDashboard2pointO: React.FC<Props> = () => {
             Current Points: <strong>{totalPoints}</strong>
           </p>
           <p className="card-text"></p>
-=======
-
-    let UID = sessionStorage.getItem("UID");
-    let token = sessionStorage.getItem("token");
-
-
-    const [personalInfo, setPersonalInfo] = React.useState<IPersonalInfo>(defaultInfoState);
-
-    const [totalPoints, setTotalPoints] = useState(0);
-    const [totalPointsAvailable, setTotalPointsAvailable] = useState([]);
-    const [userItems, setUserItems] = useState([]);
-    const [itemsSortedByPercentOfTotalPoints, setItemsSortedByPercentOfTotalPoints] = useState([]);
-    const [currentItem, setCurrentItem] = useState("Not Working");
-
-
-
-
-    React.useEffect(() => {
-        fetch(`/api/earnableItems/123`, {
-            headers: { Authorization: `Bearer ${token}` },
-        })
-            .then((res) => res.json())
-            .then((results: Array<Object> | any) => {
-                setUserItems(results);
-                (function () {
-                    results.sort((a: any, b: any) => {
-                        return a.percentage_of_total_points_needed - b.percentage_of_total_points_needed;
-                    })
-
-                    setItemsSortedByPercentOfTotalPoints(results);
-                }());
-
-            });
-    }, []);
-
-    React.useEffect(() => {
-        fetch(`/api/personal_info/person/${UID}`, {
-            headers: { Authorization: `Bearer ${token}` },
-        })
-            .then((res) => res.json())
-            .then((results) => {
-                setPersonalInfo(results[0]);
-            });
-    }, []);
-
-    // These functions in combination with the SetStates get the potential and total 
-    // grade of the wrestlers
-
-    React.useEffect(() => {
-        fetch(`/api/grades/allCurrentGradesForASingleWrestler/3`, {
-            headers: { Authorization: `Bearer ${token}` },
-        })
-            .then((res) => res.json())
-            .then((results) => {
-                const totalPointsForSetter = results.reduce((total, currentValue) => {
-                    total = total + currentValue.grade;
-                    return total;
-                }, 0)
-                const totalPointsAvailableForSetter = results.reduce((total, currentValue) => {
-                    total = total + currentValue.maximum_grade;
-                    return total;
-                }, 0)
-
-
-                return {
-                    totalPointsFrom: totalPointsForSetter,
-                    totalPointsAvailableFrom: totalPointsAvailableForSetter,
-                };
-            }).then(async ({ totalPointsFrom, totalPointsAvailableFrom }) => {
-
-                setTotalPoints(totalPointsFrom);
-                setTotalPointsAvailable(totalPointsAvailableFrom);
-
-            }
-            )
-    }, []);
-
-    React.useEffect(() => {
-        // console.log('For Loop Is Here')
-        // console.log({ personalInfo });
-        // console.log({ totalPointsAvailable });
-        // console.log({ totalPoints });
-        // console.log({ userItems });
-        // console.log({ currentItem })
-        // console.log({ userItems });
-        // console.log({ itemsSortedByPercentOfTotalPoints });
-
-        for (let theIndex = 0; theIndex < itemsSortedByPercentOfTotalPoints.length; theIndex++) {
-            const element = itemsSortedByPercentOfTotalPoints[theIndex];
-            if (element.percentage_of_total_points_needed > totalPoints) {
-                setCurrentItem(`${itemsSortedByPercentOfTotalPoints[theIndex - 1].item_color} ${itemsSortedByPercentOfTotalPoints[theIndex - 1].item_name} `)
-                break;
-            }
-            else if (element.percentage_of_total_points_needed = totalPoints) {
-                setCurrentItem(`${element}.item_color} ${element}.item_name} `)
-                break;
-            }
-        }
-    }, [totalPointsAvailable])
-
-    return (
-        <div>
-            <h1 className="text-center">Hello</h1>
-            <div className="card">
-                <h5 className="card-header">
-                    Wrestler Dashboard - {personalInfo.first_name} {personalInfo.last_name}
-                </h5>
-                <div className="card-body">
-                    <ul>
-                        {
-                            userItems.map((item) => {
-                                return (<li key={item.id}>
-                                    <h6 key={`${item.id}: PercentTotal: ${item.percentage_of_total_points_needed} `}>
-                                        {item.item_color}  {item.item_name} ({Math.round(Number(totalPointsAvailable) * (item.percentage_of_total_points_needed / 100))} points)
-                                    </h6>
-                                </li>)
-                            }
-                            )
-                        }
-                    </ul>
-                    <p className="card-text">
-                        Current Item: <strong>{currentItem}</strong>
-                        <br />
-                        Current Points: <strong>{totalPoints}</strong>
-                    </p>
-                    <p className="card-text">
-                    </p>
-                </div>
-            </div>
-
->>>>>>> 91924a3dd8cb1c7a7cb6a759f4650bd0ce97aa2c
         </div>
       </div>
     </div>
